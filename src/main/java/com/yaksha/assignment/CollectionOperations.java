af70c054c35e6c018b2134ef13889ba8e9c99099
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-04-27 19:41:22 +0530

    Update CollectionOperations.java

--- src/main/java/com/yaksha/assignment/CollectionOperations.java
+++ src/main/java/com/yaksha/assignment/CollectionOperations.java
@@ -8,27 +8,13 @@ public class CollectionOperations {
 
 	public static void main(String[] args) {
 		// **Creating a List (ArrayList)**
-		List<String> list = new ArrayList<>();
 
 		// **Adding items to the List using add()**
-		list.add("Apple");
-		list.add("Banana");
-		list.add("Cherry");
-		System.out.println("List after adding items: " + list);
 
 		// **Removing an item using remove()**
-		list.remove("Banana");
-		System.out.println("List after removing 'Banana': " + list);
 
 		// **Looping through the List using an iterator**
-		System.out.println("Looping through the List:");
-		Iterator<String> iterator = list.iterator();
-		while (iterator.hasNext()) {
-			System.out.println(iterator.next());
-		}
 
 		// **Finding the size of the List using size()**
-		int size = list.size();
-		System.out.println("Size of List: " + size);
 	}
 }
